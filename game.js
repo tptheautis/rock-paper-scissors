50ebe593cfa470e5c5e65a8f1290edec7c618708
Theautis <116971196+tptheautis@users.noreply.github.com>
2022-12-21 19:58:54 -0800

    adding console.log statements

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+//computer randomizer
 let getComputerChoice = Math.random();
 if (getComputerChoice < .33) {
     console.log('rock');
@@ -6,3 +7,31 @@ if (getComputerChoice < .33) {
 } else {
     console.log('scissors');
 }
+
+if (playerSelection = 'rock') {
+    let playSelection = .33
+} else if (playerSelection = 'paper') {
+    let playSelection = .66
+} else if (playerSelection = 'scissors') {
+    let playSelection = .99
+}
+
+//declaring winner
+function playRound(playerSelection, getComputerChoice) {
+    if ((playerSelection.toLowerCase() = 'rock') & (computerSelection = 'paper')) {
+        return
+
+        console.log('You Lose! Paper beats Rock')
+        console.log('You Lose! Scissors beats paper')
+        console.log('You lose! Rock beats scissors')
+        console.log('You win! Paper beats Rock')
+        console.log('You win! Scissors beats paper')
+        console.log('You win! Rock beats scissors')
+    }
+}
+
+function game (playRound) {
+    for (let i = 0; i < 5; i++)
+}
+const playSelection = "rock";
+
