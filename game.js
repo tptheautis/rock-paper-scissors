3fbbbfb637f466972a9c17cf2fed42dbb87178ba
Theautis <116971196+tptheautis@users.noreply.github.com>
2022-12-30 14:36:18 -0800

    swapped the onclick() function button method for getElementById in Js. Opted for a new computer randomizer. Also fixed player selection input by including window.onload function, getElementById was execututing before the DOM fully loaded

--- game.js
+++ game.js
@@ -1,72 +1,83 @@
 //start
+window.onload
 let playerScore = 0;
 let computerScore = 0;
+console.log(playerScore);
+console.log(computerScore);
+//UI
 
-var playerscored = document.querySelector('.playerscored');
-playerscored.innerHTML = playerScore;
+//playerselection
+window.onload=function() {
+    const rockBtn = document.getElementById('rockBtn');
+    const paperBtn = document.getElementById('paperBtn');
+    const scissorsBtn = document.getElementById('scissorsBtn');
 
-//Computer Randomizer
+    rockBtn.addEventListener('click', () => btn('rock'));
+    paperBtn.addEventListener('click', () => btn('paper'));
+    scissorsBtn.addEventListener('click', () => btn('scissors'));
+
+    function btn(playerSelection) {
+        const computerSelection = randomChoice();
+        playRound(playerSelection, computerSelection);
+        updateChoices(playerSelection, computerSelection);
+    }
+
+    function updateChoices(playerSelection, computerSelection) {
+
+    }
+}
+//Computer Choice Randomizer
 function randomChoice() {
-    let randomNumber = Math.floor(Math.random() * 3)
-    switch (randomNumber) {
+    let random = Math.floor(Math.random() * 3);
+    switch (random) {
         case 0:
-            return 'Rock'
-        case 1: 
-            return 'Paper'
+            return 'rock'
+        case 1:
+            return 'paper'
         case 2: 
-            return 'Scissors'
+            return 'scissors'
     }
 }
 
-//button round system
-if (playerScore == 5 || computerScore === 5) {
-    whoisthewinner();
-}
-
-function whoisthewinner() {
-    if (playerScore > computerScore) {
-        console.log('You win!')
-    } else {
-        console.log('You lose!')
-    }
-}
-//if loop
+//Rounds
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        console.log('Tie!')
+    if (playerSelection === computerSelection) {
+        console.log('tie');
     } else if (
-        (playerSelection == "rock" && computerSelection == "scissors") ||
-        (playerSelection == "paper" && computerSelection == "rock") ||
-        (playerSelection == "scissors" && computerSelection == "paper")
-    ){
-        playerScore = playerScore ++;
-    } else {
-        computerScore = playerScore ++;
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper') )
+    {    
+        playerScore++;
+        winner = 'player'
+        console.log('player won')
+
+    } else if(
+        (playerSelection === 'rock' && computerSelection === 'paper') ||
+        (playerSelection === 'paper' && computerSelection === 'scissors') ||
+        (playerSelection === 'scissors' && computerSelection === 'rock') )
+    {
+        computerScore++;
+        winner = 'computer'
+        console.log('computer won')
+    
     }
 }
-//scoring
 
-
-//rock-paper-scissors button functions
-
-function rock() {
-    let playerSelection = 'rock';
-    console.log('player selected rock')
-}
-
-function paper() {
-    let playerSelection = 'paper';
-    console.log('player selected paper');
-}
-
-function scissors() {
-    let playerSelection = 'scissors';
-    console.log('player selected scissors');
+function playGame() {
+    if (playerScore == 5) {
+        playerScore = 0;
+        computerScore = 0;
+        console.log('You win the game!')
+    } else if (computerScore == 5) {
+        playerScore = 0;
+        computerSCore = 0;
+        console.log('you lose')
+    }
 }
 
 //Restart
 function restart() {
     playerScore = 0;
     computerScore = 0;
-
 }
